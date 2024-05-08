32a4a1baf9d413f3c5a64539ca264d783d0cc0ca
Delfrinando Pranata <delfrinando@Delfrinandos-MacBook-Pro.local>
2024-05-08 19:19:15 +0700

    add unit testing

--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const { firsthandler } = require('./index')
+const { firsthandler, secondhandler } = require('./index')
 
 describe('firsthandler', () => {
   it('should return status code 200 and the correct message', async () => {
@@ -10,6 +10,20 @@ describe('firsthandler', () => {
       }, null, 2),
     };
     const result = await firsthandler(event);
+    expect(result).toEqual(expectedResponse);
+  });
+});
+
+describe('secondhandler', () => {
+  it('should return status code 200 and the correct message', async () => {
+    const event = {};
+    const expectedResponse = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "This is post function!",
+      }, null, 2),
+    };
+    const result = await secondhandler(event);
     expect(result).toEqual(expectedResponse);
   });
 });
